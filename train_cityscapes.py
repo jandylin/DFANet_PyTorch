8226e234ee19a13fee8320ab880d9890ed98ad3c
Jihao Andreas Lin <jihao.lin@web.de>
2019-06-06 15:44:47 +0200

    changed number of epoch

--- train_cityscapes.py
+++ train_cityscapes.py
@@ -26,11 +26,11 @@ parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=650, type=int, metavar='N',
-                    help='number of total epochs to run') # how many epochs? 40k iterations?
+parser.add_argument('--epochs', default=500, type=int, metavar='N',
+                    help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=48, type=int,
+parser.add_argument('-b', '--batch-size', default=32, type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
                          'batch size of all GPUs on the current node when '
